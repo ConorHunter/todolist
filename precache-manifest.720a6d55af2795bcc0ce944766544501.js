efe6138f86d6e75a28c0823a728697c5702cd772
Conor <chunter668@hotmail.com>
2020-09-17 22:16:01 +0100

    Updates

---
+++ precache-manifest.720a6d55af2795bcc0ce944766544501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3020d3e67b48f4fa9371b4b8378a19c0",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9385e691cee17ee3bb08",
+    "url": "/todolist/static/css/main.c1fff357.chunk.css"
+  },
+  {
+    "revision": "5ff1e4c0a934d28505c9",
+    "url": "/todolist/static/js/2.4b8e76c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.4b8e76c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9385e691cee17ee3bb08",
+    "url": "/todolist/static/js/main.4bb99937.chunk.js"
+  },
+  {
+    "revision": "d5b95c032eb52f9a3831",
+    "url": "/todolist/static/js/runtime-main.ae633ca2.js"
+  }
+]);
